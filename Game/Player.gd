34c8cb9682ad204baec36f3fe02b030848a1ac8a
Calin Timofte <calin.timofte@yahoo.com>
2020-11-28 21:06:17 +0200

    Style: Update animations on character and basic tiles.

--- Game/Player.gd
+++ Game/Player.gd
@@ -5,8 +5,9 @@ var moveSpeed : int = 250
 var vel : Vector2 = Vector2()
 var facingDir : Vector2 = Vector2()
 
+onready var anim = get_node("AnimatedSprite")
+
 func _physics_process(delta):
-	
 	vel = Vector2()
 	
 	# inputs
@@ -28,3 +29,30 @@ func _physics_process(delta):
 	
 	# move the player
 	move_and_slide(vel * moveSpeed)
+	
+	manage_animations()
+
+
+func manage_animations():
+	if vel.x > 0:
+		play_animation("MoveRight")
+	elif vel.x < 0:
+		play_animation("MoveLeft")
+	elif vel.y < 0:
+		play_animation("MoveUp")
+	elif vel.y > 0:
+		play_animation("MoveDown")
+	elif facingDir.x == 1:
+		play_animation("IdleRight")
+	elif facingDir.x == -1:
+		play_animation("IdleLeft")
+	elif facingDir.y == -1:
+		play_animation("IdleUp")
+	elif facingDir.y == 1:
+		play_animation("IdleDown")
+		
+func play_animation(anim_name):
+	if anim.animation != anim_name:
+		anim.play(anim_name)
+	
+	
